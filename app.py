f87d9b2673ebc0a789925a72643c27d1b51abdd3
Saranya Nayak <san00kam1@proton.me>
2026-06-24 21:14:34 +0530

    Implement signup and login functionality with corresponding HTML templates

--- app.py
+++ app.py
@@ -1,72 +1,41 @@
-from  flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET']) # / == http://10.235.72.35:5001
+@app.route('/', methods=['GET']) 
 def landing_page():
-    return render_template('landing_page.html', user_name="Kevin")
+    return render_template('landing_page.html')
 
-@app.route('/sum', methods=['GET'])
-def get_sum():
-    a = 2
-    b = 7
-    return str(a + b)   # str() converts int → string
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template('signup.html')
+    
+    elif request.method == "POST":
+        username = request.form['u_name']
+        email = request.form['e_mail']
+        password = request.form['p_word']
 
-# EXAMPLE 3 — Dynamic URL (variable in the URL)
-# <name> captures whatever the user types in the URL
-# Visit: http://localhost:5001/greet/Kevin
-# Visit: http://localhost:5001/greet/Priya
-# -------------------------------------------------------
-@app.route('/greet/<name>', methods=['GET'])
-def greet(name):
-    return f"Hello, {name}! Welcome to Flask."
+        print(f"\n\n username: {username} ,  email: {email}, password: {password} \n\n")
+        return redirect(url_for('login', message = 'Signup Successful!!!'))
 
-# EXAMPLE 4 — URL Calculator (two numbers in the URL)
-# <int:a> tells Flask to only accept whole numbers
-# Visit: http://localhost:5001/add/10/25
-# Visit: http://localhost:5001/multiply/6/7
-# Visit: http://localhost:5001/power/2/10
-# -------------------------------------------------------
-@app.route('/add/<int:a>/<int:b>', methods=['GET'])
-def add(a, b):
-    return f"{a} + {b} = {a + b}"
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        message = request.args.get('message', '') 
+        return render_template('login.html',  signup_message = message)
+    elif request.method == "POST":
+        email = request.form['e_mail']
+        password = request.form['p_word']
+
+        print(f"\n\n email: {email}, password: {password} \n\n")
+
+        return "Login Successful"
  
-@app.route('/multiply/<int:a>/<int:b>', methods=['GET'])
-def multiply(a, b):
-    return f"{a} x {b} = {a * b}"
- 
-@app.route('/power/<int:base>/<int:exp>', methods=['GET'])
-def power(base, exp):
-    return f"{base}^{exp} = {base ** exp}"
 
-
-# EXAMPLE 5 — JSON response (how real APIs work)
-# jsonify() converts a Python dict into JSON
-# Visit: http://localhost:5001/api/student/Kevin
-# Visit: http://localhost:5001/api/status
-# -------------------------------------------------------
-@app.route('/api/student/<name>', methods=['GET'])
-def get_student(name):
-    data = {
-        "name":   name,
-        "course": "Mobile App Dev",
-        "year":   2,
-        "passed": True
-    }
-    return jsonify(data)
-
-# EXAMPLE 6 - Check if a number is prime or not 
-@app.route('/isprime/<int:n>', methods=['GET'])
-def is_prime(n):
-    if n < 2:
-        return f"{n} is NOT prime"
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return f"{n} is NOT prime"
-    return f"{n} IS prime"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5001)
 
